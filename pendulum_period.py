5ac32d2222753e80318d5ca1867a8fceaa3d4476
VVagif <49620179+VVagif@users.noreply.github.com>
2024-12-04 23:54:13 +0100

    Update pendulum_period.py
    Updated Pendulum Period, takes the data directly from the user, not from the pre-defined array.

--- pendulum_period.py
+++ pendulum_period.py
@@ -4,9 +4,8 @@ def pendulum_period(lenght):
     g = 9.8 # Acceleration due to gravity
     return 2 * math.pi * math.sqrt(lenght/g)
 
-lenght = [0.5,1.0,1.5,2.0,3.5,4.0,4.5,5.0,5.5,6.0,6.5,7.0,8.0,9.0,10.0] #pendulum lenght range
-period = [pendulum_period(L) for L in lenght]
+lenght = int(input("Enter the lenght of the pendulum: ")) #pendulum lenght range
+period = pendulum_period(lenght)
 
-for L, T in zip(lenght, period):
-    print(f"Pendulum lenght: {L} m, Period : {T:.2f} s")
+print(f"Pendulum lenght: {lenght} m, Period : {period:.2f} s")
 
